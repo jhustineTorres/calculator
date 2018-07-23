0d47170a26044f8c02e3ce44ec5df932b246de93
Jhustine Torres <41136165+jhustineTorres@users.noreply.github.com>
2018-07-23 10:48:11 +0800

    Add files via upload

---
+++ js/calcu.js
@@ -0,0 +1,13 @@
+function dis(x){
+    form.answer.value=form.answer.value+x;
+}
+
+//clear textbox
+function clean(){
+    form.answer.value='';
+}
+
+//dellllllllllllls
+function del(){
+    form.answer.value=form.answer.value.slice(0,form.answer.value.length-1);
+}
